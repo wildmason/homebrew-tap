fbdd807db1460b7781edaeeae03b1979c0381d8d
Matthew MacKinnon <matthewmackinnon@gmail.com>
2026-05-20 09:12:07 -0300

    Update Prosaic to 1.0.1

--- Formula/prosaic.rb
+++ Formula/prosaic.rb
@@ -1,14 +1,14 @@
 class Prosaic < Formula
   desc "Natural language generation from JSON-lines events"
   homepage "https://github.com/wildmason/prosaic"
-  version "1.0.0"
+  version "1.0.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on :linux
   depends_on arch: :x86_64
 
-  url "https://github.com/wildmason/prosaic/releases/download/v1.0.0/prosaic-v1.0.0-x86_64-unknown-linux-gnu.tar.gz"
-  sha256 "f8f1da96d2a01106dc42d2f6aa8c68510f190a6dee7935c9f5d7bec3133fafb6"
+  url "https://github.com/wildmason/prosaic/releases/download/v1.0.1/prosaic-v1.0.1-x86_64-unknown-linux-gnu.tar.gz"
+  sha256 "19e49a3b22a1589e5f0d4fb7df10f48273b02f15b4de4813a7a78d1367ea6542"
 
   def install
     bin.install "prosaic"
